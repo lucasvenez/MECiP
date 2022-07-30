b3a8b0e9806c95d747ac491756761fde72d20526
Lealfelipe <108575891+Lealfelipe@users.noreply.github.com>
2022-07-30 09:04:20 -0300

    Add files via upload
    Códigos Microblog

--- mecip/__init__.py
+++ mecip/__init__.py
@@ -1,2 +1,11 @@
-from mecip.index_extractor import ProfessorIndexExtractor
-from mecip.xml_parser import process_file
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes
